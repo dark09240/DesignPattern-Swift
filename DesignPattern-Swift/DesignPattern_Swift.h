314140787caebc98af8f2782615850c54665a9e0
Dong <dark09240@gmail.com>
2023-04-20 10:21:04 +0800

    Create project.

---
+++ DesignPattern-Swift/DesignPattern_Swift.h
@@ -0,0 +1,18 @@
+//
+//  DesignPattern_Swift.h
+//  DesignPattern-Swift
+//
+//  Created by Dong on 2023/4/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DesignPattern_Swift.
+FOUNDATION_EXPORT double DesignPattern_SwiftVersionNumber;
+
+//! Project version string for DesignPattern_Swift.
+FOUNDATION_EXPORT const unsigned char DesignPattern_SwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DesignPattern_Swift/PublicHeader.h>
+
+
